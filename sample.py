4abd7db9820f637ca1a877b12f45f3a73cd82504
krish77290 <42002445+krish77290@users.noreply.github.com>
2019-02-09 12:03:43 +0530

    some changing in sample.py

--- sample.py
+++ sample.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+#from flask import Flask
 app = Flask(__name__)
+ 
+@app.route('/result',methods = ['POST'])
+def result():
 
+    city =  request.form["city"]
+    city = reverse_string(city)
+    return render_template("result.html",result = city)
 
-@app.route('/test') #rest api
+@app.route('/') #rest api
 def hello():
-    return "Hello World!"
+    return render_template('test.html')
+
+def reverse_string(content):
+    return content[::-1]
 
 if __name__ == '__main__':
     app.run()
